87c63850b1bbf13a84bb5bd260bf35b834224dcf
inoriii <1873845089@qq.com>
2022-07-07 00:09:48 +0800

    使用docker-compose,配置动态ip主从

--- docker/mysql5.7/slave/init/init.sql
+++ docker/mysql5.7/slave/init/init.sql
@@ -1,4 +1,4 @@
-change master to master_host ='173.18.32.2',master_port =3306,master_user ='slave',master_password ='123456';
+change master to master_host ='mysql-master',master_port =3306,master_user ='slave',master_password ='123456';
 
 CREATE USER 'inoriii'@'%' IDENTIFIED BY '123456';
 GRANT All privileges ON `hello_sring`.* TO 'inoriii'@'%';
