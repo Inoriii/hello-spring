18251715eb201194bcc034114bc044b671fd12a2
inoriii <1873845089@qq.com>
2022-07-19 23:29:31 +0800

    Validated参数校验demo2，优化全局异常处理

--- hello-spring-web/src/main/java/com/inoriii/hello/spring/web/handler/GlobalExceptionHandler.java
+++ hello-spring-web/src/main/java/com/inoriii/hello/spring/web/handler/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.inoriii.hello.spring.web.handler;
 import com.inoriii.hello.spring.api.enums.ResponseCode;
 import com.inoriii.hello.spring.api.vo.RestResult;
 import com.inoriii.hello.spring.common.exception.BusinessException;
-import org.springframework.validation.ObjectError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,8 +15,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -23,7 +27,48 @@ import java.util.stream.Collectors;
  * @description:
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    /**
+     * PostMapping:
+     * BindException:Content-Type:
+     * Content-Type: application/x-www-form-urlencoded 表单提交
+     * MethodArgumentNotValidException:
+     * Content-Type: application/json requestBody为json
+     * ConstraintViolationException：
+     * Content-Type: application/x-www-form-urlencoded 单参数校验
+     */
+    public static final Map<Class<? extends Exception>, Function<Exception, String>> VALIDATION_ERROR_MAP =
+            new ConcurrentHashMap<Class<? extends Exception>, Function<Exception, String>>(8) {{
+                put(ConstraintViolationException.class,
+                        k -> ((ConstraintViolationException) k).getConstraintViolations().stream()
+                                .map(ConstraintViolation::getMessage)
+                                .collect(Collectors.joining("; ")));
+                put(MethodArgumentNotValidException.class,
+                        k -> ((MethodArgumentNotValidException) k).getBindingResult().getAllErrors().stream()
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                .collect(Collectors.joining("; ")));
+                put(BindException.class,
+                        k -> ((BindException) k).getAllErrors().stream()
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                .collect(Collectors.joining("; ")));
+            }};
+
+    @ExceptionHandler(value = {
+            ConstraintViolationException.class,
+            MethodArgumentNotValidException.class,
+            BindException.class})
+    @ResponseBody
+    public RestResult<Object> handleMissingServletRequestParameterException(Exception e) {
+        AtomicReference<String> message = new AtomicReference<>("参数错误");
+        VALIDATION_ERROR_MAP.forEach((key, value) -> {
+            if (e.getClass().isAssignableFrom(key)) {
+                message.set(value.apply(e));
+            }
+        });
+        return new RestResult<>(ResponseCode.FAIL.getCode(), message.get(), null);
+    }
+
     /**
      * 定义要捕获的异常 可以多个 @ExceptionHandler({})
      *
@@ -48,23 +93,10 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(RuntimeException.class)
     public RestResult<Object> runtimeExceptionHandler(HttpServletRequest request, final Exception e, HttpServletResponse response) {
+        log.error("error:" + e);
         return new RestResult<>(ResponseCode.FAIL);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    public RestResult<Object> handleMissingServletRequestParameterException(MethodArgumentNotValidException e, HttpServletRequest request) {
-        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
-        String message = allErrors.stream().map(s -> s.getDefaultMessage()).collect(Collectors.joining(";"));
-        return new RestResult<>(ResponseCode.FAIL.getCode(), message, null);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public RestResult<Object> constraintViolationExceptionHandler(ConstraintViolationException e) {
-        Set<ConstraintViolation<?>> allErrors = e.getConstraintViolations();
-        String message = allErrors.stream().map(o -> o.getMessage()).collect(Collectors.joining(";"));
-        return new RestResult(ResponseCode.FAIL.getCode(), message, null);
-    }
 
     /**
      * Default Exception Handler
