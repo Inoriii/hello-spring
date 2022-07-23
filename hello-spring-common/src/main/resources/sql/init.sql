077a4e62401136ead3278f62c527b8c863b7b6b5
inoriii <1873845089@qq.com>
2022-07-23 23:57:16 +0800

    spring security day1  创建用户、角色表

--- hello-spring-common/src/main/resources/sql/init.sql
+++ hello-spring-common/src/main/resources/sql/init.sql
@@ -18,3 +18,46 @@ CREATE TABLE hello_spring.`user_test`
   CHARACTER SET = utf8
   COLLATE = utf8_general_ci
   ROW_FORMAT = Dynamic;
+
+CREATE TABLE hello_spring.`user`
+(
+    `id`          int(11)                                                 NOT NULL AUTO_INCREMENT,
+    `username`    varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL COMMENT '用户名称',
+    `password`    varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户密码',
+    `enabled`     boolean                                                 NOT NULL DEFAULT TRUE COMMENT '是否可用',
+    `create_time` datetime(0)                                             NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `update_time` datetime(0)                                             NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 500001
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+CREATE TABLE hello_spring.`role`
+(
+    `id`          int(11)                                                NOT NULL AUTO_INCREMENT,
+    `role_name`   varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
+    `enabled`     boolean                                                NOT NULL DEFAULT TRUE COMMENT '是否可用',
+    `create_time` datetime(0)                                            NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `update_time` datetime(0)                                            NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 500001
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+CREATE TABLE hello_spring.`user_role`
+(
+    `id`          int(11)     NOT NULL AUTO_INCREMENT,
+    `user_id`     int(11)     NOT NULL COMMENT '用户名称',
+    `role_id`     int(11)     NOT NULL COMMENT '用户名称',
+    `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 500001
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
