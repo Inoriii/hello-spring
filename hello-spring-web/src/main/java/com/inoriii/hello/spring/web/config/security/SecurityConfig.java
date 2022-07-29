0344dc1d73a9a1180e1302c9f1b2f41abe0d451d
inoriii <1873845089@qq.com>
2022-07-29 23:29:33 +0800

    spring security day4  增加授权，验证

---
+++ hello-spring-web/src/main/java/com/inoriii/hello/spring/web/config/security/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.inoriii.hello.spring.web.config.security;
+
+import com.inoriii.hello.spring.api.service.UserService;
+import com.inoriii.hello.spring.api.vo.FetchUserRoleVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sakura
+ * @date: 2022/7/23 16:38
+ * @description:
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServiceBean() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                FetchUserRoleVO user = userService.fetchUserRoleVO(username);
+                if (user == null) {
+                    throw new UsernameNotFoundException("用户： " + username + " 不存在！");
+                }
+                List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+                List<FetchUserRoleVO.RoleVO> roles = user.getRoleList();
+                roles.forEach(r -> {
+                    grantedAuthorities.add(new SimpleGrantedAuthority(r.getRoleName()));
+                });
+                return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
+            }
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests().
+                anyRequest().authenticated().
+                and().
+                //默认表单
+                        formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers(HttpMethod.GET);
+    }
+
+}
